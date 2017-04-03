960403b74f564b08d93a9fae644306c91c096879
Мария Черняк < >
2017-04-03 23:05:24 +0300

    update

--- homework_4/1/script.js
+++ homework_4/1/script.js
@@ -25,37 +25,3 @@ row[20].innerHTML += "<td>" + window.screenX+ "</td>";
 row[21].innerHTML += "<td>" + window.screenY+ "</td>";
 row[22].innerHTML += "<td>" + window.scrollX+ "</td>";
 row[23].innerHTML += "<td>" + window.scrollY+ "</td>";
-
-	// .href //полный адрес страницы на которой находимся
-	// .host//доменное имя
-	// .protocol//
-	// .port 
-	// .search
-	// .hash
-	// .assign(s)// открывает страницу с этим адресом
-	// .reload()//обновляет страницу
-
-	// .width	//размеры экрана (не окна блаузера)
-	// .height
-	// .availWidth//размер полезной области
-	// .availHeight
-	// .colorDepth//лубина цветопередачи
-	// .orientation()//ориентация экрана
-
-	// window.navigator
-	// .appCodeName
-	// .appName 
-	// .userAgent         //тоже самое что при httml запросе
-	// .cookieEnabled //включены ли куки
-	// .javaEnabled()// доступна ли джава
-	// .onLine	// true , если в сети
-
-
-// window.innerWidth
-// 	.innerHeight//размеры области просмотра
-// 	.screenX//насколько окно бровзера отстает от левого края экрана
-// 	.screenLeft// y
-// 	.screenY 
-// 	.screenTop
-// 	.scrollX// насколько проскролена страницы
-// 	.scrollY
