4675ee54039e87df3e3b3b4cb1e08ccb54a04788
Мария Черняк < >
2017-06-02 18:56:25 +0300

    update workshop 3

--- workshop_3/script.js
+++ workshop_3/script.js
@@ -1,106 +1,195 @@
 /*
 Разработать приложение для работы на карте.
 */
-var arrCategories; //переменная, в которой будет храниться массив категорий
-var currentCategory;// выбранная категория
-var mapBlock = document.querySelector('.map');
-var map;
 
-navigator.geolocation.getCurrentPosition(function(d){ 
-	var coords = new google.maps.LatLng(d.coords.latitude, d.coords.longitude) 
-	var setting = { 
-		zoom: 6, 
-		center : coords, 
-		mapTypeId : google.maps.MapTypeId.ROADMAP 
-	} 
-	map = new google.maps.Map(mapBlock,setting);
-});
+var arrCategories =[]; //переменная, в которой будет храниться массив категорий
+var currentCategory =[];// массив с точками выбранной категории 
+var mapBlock = document.querySelector('.map'); // блок, в котором будет находиться карта
+var map; //карта
+var currentCoord ={}; // объект с координатами текущего местоположения
+var infoWindowArray =[]; //массив информационных окон к выбранной категории
+var markersArray =[]; // массив маркеров выбранной категории
+var route = []; // массив точек маршрута
+var distanceRoute = 0;
+
 
 var xhr = new XMLHttpRequest();
 
 xhr.open("GET", "json.json" , true); 
 xhr.onload = function(){
 	arrCategories = JSON.parse(xhr.responseText);
+	navigator.geolocation.getCurrentPosition(function(d){ 
+		var coords = new google.maps.LatLng(d.coords.latitude, d.coords.longitude);
+		currentCoord.coordslat = d.coords.latitude;
+		currentCoord.coordslng = d.coords.longitude;
+		route.push(currentCoord);
+				
+		var setting = { 
+			zoom: 6, 
+			center : coords, 
+			mapTypeId : google.maps.MapTypeId.ROADMAP 
+		} 
 
-	openListPoints(arrCategories[1]);
+			createListCategory();
+		map = new google.maps.Map(mapBlock,setting);
+		var marker = new google.maps.Marker({
+		    position: coords,
+		    map: map,
+		    icon: "marker.png",
+		    title: "Вы находитесь тут"
+		  });
+		openListPoints(arrCategories[1]);
 
-	// var markers = [];
-	// var distanse = 0;
-	// var coordsArray =[];
-	// var coords = new google.maps.LatLng(currentCategory.coordslat, currentCategory.coordslng);
-	// var setting = {
-	// 	zoom: 7,
-	// 	center: coords,
-	// 	mapTypeId: google.maps.MapTypeId.ROADMAP
-	// }
-	// var map = new google.maps.Map(mapBlock, setting);
+	});
 
-
-	// for(var i = 0; i< arrCategories.length; i++){
-	// 	var coords = new google.maps.LatLng(arrCategories[i].x, arrCategories[i].y)
-	// 	coordsArray.push(coords);
-	// 	markers[i] = new google.maps.Marker({
-	// 		position: coords,
-	// 		map: map,
-	// 		title: arrCategories[i].name
-	// 	})
-	// 	if(i < arrCategories.length-1){
-	// 		distanse += calcDistanse(arrCategories[i].x, arrCategories[i].y, arrCategories[i+1].x, arrCategories[i+1].y);
-	// 	}	
-	// }
-
-
-
-  // var contentString = "<div class='content'<img src='>" + currentCategory.img +"'><h3>"+ currentCategory.name +"</h3><p>"+ currentCategory.text +  '</p></div>';
-
-  // var infowindow = new google.maps.InfoWindow({
-  //   content: contentString
-  // });
-
-  // var marker = new google.maps.Marker({
-  //   position: coords,
-  //   map: map,
-  //   title: 'Uluru (Ayers Rock)'
-  // });
-  // marker.addListener('click', function() {
-  //   infowindow.open(map, marker);
-  // });
 }
 xhr.send();
 
 
+//отображает список категорий 
+function createListCategory(){
+	for(var i = 0; i < arrCategories.length; i++){
+		var li = document.createElement('li');
+		li.innerHTML = "<a href='#' role='button' id='" + i + "'>" + arrCategories[i].name + "</a>";
+
+		li.querySelector('a').addEventListener('click', function(e){
+			e.preventDefault();
+			openListPoints(arrCategories[this.id])
+		})
+		document.querySelector('.category').appendChild(li);
+	}
+}
+
+//перенос центра карты в выбранную точку
+function zoomIn(){
+	var id = parseInt(this.id);
+	map.setCenter(new google.maps.LatLng(currentCategory[id].coordslat, currentCategory[id].coordslng));
+	for(var i = 0; i < infoWindowArray.length; i++){
+		infoWindowArray[i].close();
+	}
+	infoWindowArray[id].open(map, markersArray[id]);
+	 document.querySelector('.buttonAddition').addEventListener('click', addPointToRoute);
+}
+
+//добавление точки в маршрут
+function addPointToRoute(){
+
+	var id = parseInt(this.id);
+	route.push(currentCategory[id]);
+	var li = document.createElement('li');
+	li.innerHTML = "<h3>" + currentCategory[id].name + "</h3><p>" + currentCategory[id].text + "</p>";
+	var length = route.length;
+	var distance = calcDistanse(route[length - 2].coordslat, route[length - 2].coordslng, route[length - 1].coordslat, route[length - 1].coordslat );
+	distanceRoute += distance;
+	li.innerHTML += "<span>" + distance + "km</span>";
+	document.querySelector('.route').appendChild(li);
+	drawRoute(route[length - 2] , route[length - 1]);
+	document.querySelector('.distance').innerHTML = distanceRoute + ' km';
+	
+}
+//рисование пути между двумя точками. Принимает 2 объекта точек
+function drawRoute(first, second){
+	
+	var flightPlanCoordinates = [
+	    {lat: first.coordslat, lng: first.coordslng},
+	    {lat: second.coordslat, lng: second.coordslng}
+  	];
+  var flightPath = new google.maps.Polyline({
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
+
+  flightPath.setMap(map);
+}
+
+//вывод точек выбранной категории. Принимает массив точек
+function showListPoints(currentCategory){
+	var blockCurrentCategory = document.querySelector('.currentCategory');
+	blockCurrentCategory.innerHTML = '';
+	for(var i = 0; i < currentCategory.length; i++){
+		var li = document.createElement('li');
+		li.innerHTML = "<a href='#' role='button' id='" + i + "c'>" + currentCategory[i].name + "</a>";
+		li.querySelector('a').addEventListener('click', zoomIn);
+		blockCurrentCategory.appendChild(li);
+	}
+}
+//добавлеет точкам поле с расстоянием до текущего положени и сортирует по возрастанию длины
+function sortPoints(){
+	for (var i = 0; i < currentCategory.length; i++) {
+		currentCategory[i].distance = calcDistanse(currentCoord.latitude, currentCoord.longitude, currentCategory[i].coordslat, currentCategory[i]. coordslng)
+	}
+	currentCategory.sort(function( a, b ){
+		if (a.distance > b.distance){
+			return 1;
+		}
+		else{
+			return -1;
+		}
+	})
+}
+
+//Принимает объект из массива категорий. Открывает Список точек
 function openListPoints(object){
-	console.log(object);
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET", object.path , true);
 	xhr.onload = function(){
 		currentCategory = JSON.parse(xhr.responseText);
-		console.log(currentCategory);
+		sortPoints();
 		showPoints(currentCategory);
+		showListPoints(currentCategory);
 	} 
 	xhr.send();
 	
 }
-function showPoints(currentCategory){
-	
-	for(var i = 0; i < currentCategory.length; i++){
-		var contentString = "<div class='content'<img src='>" + currentCategory[i].img +"'><h3>"+ currentCategory[i].name +"</h3><p>"+ currentCategory[i].text +  '</p></div>';
 
-	  var infowindow = new google.maps.InfoWindow({
-	    content: contentString
-	  });
-	  var coords = new google.maps.LatLng(currentCategory[i].coordslat, currentCategory[i].coordslng);
-	  console.log(currentCategory[i]);
-	  var marker = new google.maps.Marker({
-	    position: coords,
-	    map: map
-	  });
-	  console.log(map)
-	  marker.addListener('click', function() {
-	    infowindow.open(map, marker);
-	  });
+//Очищает массив маркеров, убирает их со страницы
+function deleteMarkers(){
+	for(var i = 0; i < markersArray.length; i++){
+		markersArray[i].setMap(null);
+	}
+	markersArray.length = 0; 
+	infoWindowArray.length = 0;
+
+}
+
+function showPoints(currentCategory){
+	deleteMarkers();
+	for(var i = 0; i < currentCategory.length; i++){
+		createInfoWindow(currentCategory[i], i);
 	}
 }
+//добавляет обработчик для появления информационного окна
+function showInfoWindow(marker, infoWindow){
+	 marker.addListener('click', function() {
+		  	for(var i = 0; i < infoWindowArray.length; i++){
+		  		infoWindowArray[i].close();
+		  	}
+		    infoWindow.open(map, this);
+		    document.querySelector('.buttonAddition').addEventListener('click', addPointToRoute);
+	 });
+}
+
+//создает информационное окно. Принимает индекс точки
+function createInfoWindow(currentCategory, i){
+	var contentString = "<div class='content'><img src='" + currentCategory.img +"'><h3>"+ currentCategory.name +"</h3><p>"+ currentCategory.text +  '</p></div>';
+	contentString += "<button id='" +  i + "b' type ='button'  class='buttonAddition'>Добавить в маршрут</button>"
+	var infoWindow = new google.maps.InfoWindow({
+	  	  content: contentString
+	});
+	 	infoWindowArray.push(infoWindow);
+		 var coords = new google.maps.LatLng(currentCategory.coordslat, currentCategory.coordslng);
+		 var marker = new google.maps.Marker({
+		    position: coords,
+		    map: map
+		 });
+	markersArray.push(marker);
+
+	showInfoWindow(marker, infoWindow);
+}
+
 
 //Функция для расчета расстояния между двумя точками на карте
 function calcDistanse (lat1, lng1, lat2, lng2){ 
